3424d5d1a3a7edbc726deefb9da336b34d738b4f
Anas Malik <anasmalik081@gmail.com>
2022-02-23 17:27:04 +0530

    django.contrib.messages error fix

--- MyReports/settings.py
+++ MyReports/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',,
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
     'phonenumber_field',
