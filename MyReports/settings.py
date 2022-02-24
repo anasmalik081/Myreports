6a7838e29fbec80bc805d2eaa3a3afab3f1bf485
Anas Malik <anasmalik081@gmail.com>
2022-02-24 19:01:41 +0530

    dbsqlite

--- MyReports/settings.py
+++ MyReports/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+
 import os
 from pathlib import Path
 from django.contrib.messages import constants as message_s
 from django.core.management.utils import get_random_secret_key
-from .import myip
+from . import myip
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
