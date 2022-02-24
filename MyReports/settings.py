1a00b5b886466124d9d6c1021d9d1ee71dc96c32
Anas Malik <anasmalik081@gmail.com>
2022-02-24 22:13:04 +0530

    Templates Problem

--- MyReports/settings.py
+++ MyReports/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'MyReports.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
