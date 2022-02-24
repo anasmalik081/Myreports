bc06c7a6844ab929459a5a9b95e71bf689f13fcc
anasmalik081 <84465546+anasmalik081@users.noreply.github.com>
2022-02-24 20:08:20 +0530

    static_root updated

--- MyReports/settings.py
+++ MyReports/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 # print("********************:::",BASE_DIR / "static/")
 
-STATIC_ROOT = BASE_DIR / "staticfiles/"
+STATIC_ROOT = "/var/www/myapp/static"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
