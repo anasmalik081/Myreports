e63e4d33df8d688f5b7bea5d214fc6aed47a2fd0
anasmalik081 <84465546+anasmalik081@users.noreply.github.com>
2022-02-24 20:04:48 +0530

    updates

--- MyReports/settings.py
+++ MyReports/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', myip.local_ip]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', myip.external_ip]
 # ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 # AUTH_USER_MODEL = 'Apps.User'
 DEBUG = True
